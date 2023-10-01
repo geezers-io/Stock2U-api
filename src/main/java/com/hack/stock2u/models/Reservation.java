16bbaefffd2c8ec2fccc108ab5e1bee3ce552c77
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-02 02:16:55 +0900

    feat: 예약 엔티티 작성

---
+++ src/main/java/com/hack/stock2u/models/Reservation.java
@@ -0,0 +1,45 @@
+package com.hack.stock2u.models;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "reservations")
+public class Reservation {
+  @Id
+  @Column
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Comment("MongoDB Chat 식별자")
+  @Column(name = "chat_id")
+  private String chatId;
+
+  @Comment("판매자(사업자) id")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "seller_id")
+  private User seller;
+
+  @Comment("구매자(예약자) id")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "customer_id")
+  private User customer;
+
+  @Column(name = "disabled_at")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date disabledAt;
+}
