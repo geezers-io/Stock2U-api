5246a0aadc7097a6e33b32abca9e820114178940
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-01 23:43:00 +0900

    fix: securityConfig 에러 수정

--- src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
+++ src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
@@ -3,11 +3,13 @@ package com.hack.stock2u.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfig {
   @Bean
-  public void securityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.formLogin().disable();
+    return http.build();
   }
 }
