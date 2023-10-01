10d3a7e8b9f9abbc2eed77eae2ffd5d097d2e8c9
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-02 02:17:13 +0900

    feat: 상품 이미지 엔티티 작성

---
+++ src/main/java/com/hack/stock2u/models/ProductImage.java
@@ -0,0 +1,31 @@
+package com.hack.stock2u.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "product_images")
+public class ProductImage {
+  @Id
+  @Column
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id")
+  private Product product;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+}
