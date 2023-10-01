5ecc6577fda70037f4d204d72863aabe109adc8b
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-01 22:35:18 +0900

    feat: formLogin disable

--- src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
+++ src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.hack.stock2u.global.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
 public class SecurityConfig {
+  @Bean
+  public void securityFilterChain(HttpSecurity http) throws Exception {
+    http.formLogin().disable();
+  }
 }
