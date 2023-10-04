14b0dd7580f496e70d8020f3eab982f091a3c870
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-04 15:25:11 +0900

    feat: PasswordEncoder Bean 객체 작성

--- src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
+++ src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.hack.stock2u.global.config;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -11,5 +16,14 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.formLogin().disable();
     return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    String idForEncode = "bcrypt";
+    BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
+    Map<String, PasswordEncoder> store = new HashMap<>();
+    store.put(idForEncode, bcrypt);
+    return new DelegatingPasswordEncoder(idForEncode, store);
   }
 }
