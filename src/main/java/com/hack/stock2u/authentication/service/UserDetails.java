8cbc2e0b1d9afb02bf3073bd9eeb7a9cb0ecdf14
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-05 18:17:07 +0900

    feat: 인증 관련 비즈니스 객체 작성

---
+++ src/main/java/com/hack/stock2u/authentication/service/UserDetails.java
@@ -0,0 +1,53 @@
+package com.hack.stock2u.authentication.service;
+
+import com.hack.stock2u.constant.UserRole;
+import com.hack.stock2u.models.User;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@RequiredArgsConstructor
+public class UserDetails implements org.springframework.security.core.userdetails.UserDetails {
+  private final User user;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    UserRole role = user.getRole();
+    AppGrantedAuthority authority = new AppGrantedAuthority(role);
+    return Collections.singletonList(authority);
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return user.getName();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return user.getDisabledAt() == null;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    Date removedAt = user.getBasicDate().getRemovedAt();
+    Date disabledAt = user.getDisabledAt();
+    return removedAt == null && disabledAt == null;
+  }
+}
