192866739804b46147b22487d05eb2f10392fda0
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-06 20:04:27 +0900

    feat: 컨버터 변환 계층 추가

---
+++ src/main/java/com/hack/stock2u/global/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.hack.stock2u.global.config;
+
+import com.hack.stock2u.authentication.service.AuthVendorEnumConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(new AuthVendorEnumConverter());
+  }
+
+}
