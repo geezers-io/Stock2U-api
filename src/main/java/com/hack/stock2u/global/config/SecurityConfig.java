a8fa8f0564af09984f82a6db06de6db1c4aa07f0
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-11 12:57:36 +0900

    feat: SecurityConfig 추가 내용

--- src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
+++ src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.hack.stock2u.global.config;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,13 +11,30 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.CorsRegistry;
 
 @EnableWebSecurity
 public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.formLogin().disable();
+    http.csrf().disable();
+    http.cors().configurationSource(configurationSource());
     return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource configurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedMethods(List.of("*"));
+    UrlBasedCorsConfigurationSource source =
+        new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   /**
