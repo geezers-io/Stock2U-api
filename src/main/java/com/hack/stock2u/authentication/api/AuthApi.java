273c6eb230be0eed480669adae7d8ca633104d3a
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-11 13:09:49 +0900

    feat: 로그인 API 컨트롤러 작성

--- src/main/java/com/hack/stock2u/authentication/api/AuthApi.java
+++ src/main/java/com/hack/stock2u/authentication/api/AuthApi.java
@@ -1,5 +1,7 @@
 package com.hack.stock2u.authentication.api;
 
+import com.hack.stock2u.authentication.dto.AuthRequestDto;
+import com.hack.stock2u.authentication.dto.LoginResponse;
 import com.hack.stock2u.authentication.dto.UrlJson;
 import com.hack.stock2u.authentication.service.AuthService;
 import com.hack.stock2u.constant.AuthVendor;
@@ -9,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +25,7 @@ public class AuthApi {
   private final AuthService authService;
 
   @Operation(summary = "로그인 URL API", description = "각 벤더 요청에 따른 로그인 URL 을 반환합니다.")
-  @GetMapping("/login")
+  @GetMapping("/login-url")
   public ResponseEntity<UrlJson> login(
       @Parameter(
           name = "vendor",
@@ -32,6 +36,14 @@ public class AuthApi {
   ) {
     UrlJson loginUrl = authService.getLoginUrl(vendor);
     return ResponseEntity.ok(loginUrl);
+  }
+
+  @Operation(summary = "로그인 API", description = "로그인을 수행합니다.")
+  @PostMapping("/login")
+  public ResponseEntity<LoginResponse> getTokenApi(@RequestBody AuthRequestDto.Token tokenRequest) {
+    String token = tokenRequest.token();
+    LoginResponse login = authService.login(token);
+    return ResponseEntity.ok().body(login);
   }
 
 }
