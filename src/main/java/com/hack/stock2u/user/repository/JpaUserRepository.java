ab8df76f0f037c7dcefe39d6c40a65df1f41c717
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-14 00:48:13 +0900

    feat: userRepository 추가 메서드 작성

--- src/main/java/com/hack/stock2u/user/repository/JpaUserRepository.java
+++ src/main/java/com/hack/stock2u/user/repository/JpaUserRepository.java
@@ -1,11 +1,21 @@
 package com.hack.stock2u.user.repository;
 
+import com.hack.stock2u.constant.AuthVendor;
 import com.hack.stock2u.models.User;
 import java.util.Optional;
+import javax.swing.text.html.Option;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaUserRepository extends CrudRepository<User, Long> {
   Optional<User> findByEmail(String email);
+
+  @Query("select u from users u where u.email = :email and u.vendor = :vendor")
+  Optional<User> findByEmailAndVendor(String email, AuthVendor vendor);
+
+  Optional<User> findById(Long userId);
+
+  Optional<User> findByPhone(String phone);
 }
