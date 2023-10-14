fb5a75034491ec738f62afbacd6d391969385640
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-15 00:38:18 +0900

    docs: add nullable typing

--- src/main/java/com/hack/stock2u/authentication/dto/LoginResponse.java
+++ src/main/java/com/hack/stock2u/authentication/dto/LoginResponse.java
@@ -1,3 +1,9 @@
 package com.hack.stock2u.authentication.dto;
 
-public record LoginResponse(boolean exists, String email, UserDetails user) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record LoginResponse(boolean exists,
+                            @Schema(nullable = true, description = "회원가입 페이지에서 사용할 email")
+                            String email,
+                            @Schema(nullable = true, description = "사용자 정보 객체")
+                            UserDetails user) {}
--- src/main/java/com/hack/stock2u/authentication/dto/UserDetails.java
+++ src/main/java/com/hack/stock2u/authentication/dto/UserDetails.java
@@ -4,6 +4,7 @@ import com.hack.stock2u.constant.AuthVendor;
 import com.hack.stock2u.constant.UserRole;
 import com.hack.stock2u.models.SellerDetails;
 import com.hack.stock2u.models.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
@@ -14,6 +15,7 @@ public record UserDetails(
     String phone,
     UserRole role,
     AuthVendor vendor,
+    @Schema(nullable = true, description = "판매자 정보(사용자가 판매자라면 전달 됨)")
     SellerDetails sellerDetails
 ) {
   public static UserDetails user(User user) {
