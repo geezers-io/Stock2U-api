00f8872ee88d99881874287530c0e70aca901e67
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-15 00:09:09 +0900

    feat: 범용 예외 처리 케이스 추가

--- src/main/java/com/hack/stock2u/global/exception/GlobalException.java
+++ src/main/java/com/hack/stock2u/global/exception/GlobalException.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public enum GlobalException implements BasicErrorCase {
   ILLEGAL_ARGUMENT(HttpStatus.BAD_REQUEST, "FAULT_ARG", "잘못된 인자 값입니다."),
-  SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SERVER_ERROR", "서버 에러가 발생하였습니다.");
+  SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "SERVER_ERROR", "서버 에러가 발생하였습니다."),
+  BAD_REQUEST(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "잘못된 요청입니다.");
 
   private final HttpStatus httpStatus;
   private final String errorCode;
