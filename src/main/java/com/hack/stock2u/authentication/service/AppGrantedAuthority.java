483a73da29dec15fc9846be64e25b08a0eef201d
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-15 00:09:49 +0900

    fix: 잘못된 property 를 반환하고 있던 Granted 클래스 수정

--- src/main/java/com/hack/stock2u/authentication/service/AppGrantedAuthority.java
+++ src/main/java/com/hack/stock2u/authentication/service/AppGrantedAuthority.java
@@ -2,14 +2,16 @@ package com.hack.stock2u.authentication.service;
 
 import com.hack.stock2u.constant.UserRole;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AppGrantedAuthority implements GrantedAuthority {
   private final UserRole userRole;
 
   @Override
   public String getAuthority() {
-    return userRole.getName();
+    return userRole.name();
   }
 }
