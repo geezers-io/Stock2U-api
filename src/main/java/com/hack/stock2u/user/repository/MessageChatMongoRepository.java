59a61e512beb427e05a75f615ebaed7920a4a5b6
HyukJunJi <whrnjsrkdls@gmail.com>
2023-10-16 02:17:42 +0900

    feat: 채팅 위한 jpa repository

--- src/main/java/com/hack/stock2u/user/repository/MessageChatMongoRepository.java
+++ src/main/java/com/hack/stock2u/user/repository/MessageChatMongoRepository.java
@@ -1,14 +1,8 @@
 package com.hack.stock2u.user.repository;
 
 import com.hack.stock2u.models.ChatMessage;
-import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageChatMongoRepository extends MongoRepository<ChatMessage, String> {
-
-
-
-}
+public interface MessageChatMongoRepository extends MongoRepository<ChatMessage, String> {}
