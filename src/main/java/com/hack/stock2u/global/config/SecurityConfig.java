162555400bd45737608cea90497f4fd15724a98f
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-16 03:58:47 +0900

    fix: SecurityConfig

--- src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
+++ src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowCredentials(true);
     configuration.setExposedHeaders(List.of("*"));
-    configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+    configuration.setAllowedOrigins(List.of("*"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
     UrlBasedCorsConfigurationSource source =
         new UrlBasedCorsConfigurationSource();
