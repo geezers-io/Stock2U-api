b86c0dea947e855347ce180765b7c74b813968d7
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-16 20:26:04 +0900

    fix: allow credentials rollback

--- src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
+++ src/main/java/com/hack/stock2u/global/config/SecurityConfig.java
@@ -6,15 +6,18 @@ import com.hack.stock2u.authentication.service.UserDetailService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
@@ -26,10 +29,7 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.formLogin().disable();
-    http.csrf().disable();
-    http.logout().disable();
-//    http.cors().configurationSource(configurationSource());
+    disableMvcSettings(http);
 
     http.userDetailsService(userDetailService);
     http.authenticationManager(authManager);
@@ -54,7 +54,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource configurationSource() {
     CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true);
+//    config.setAllowCredentials(true);
     config.setExposedHeaders(List.of("*"));
     config.setAllowedOriginPatterns(List.of("*"));
     config.setAllowedMethods(List.of("*"));
@@ -69,6 +69,14 @@ public class SecurityConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+
+  private void disableMvcSettings(HttpSecurity http) throws Exception {
+    http.formLogin().disable();
+    http.csrf().disable();
+    http.logout().disable();
+    http.cors().configurationSource(configurationSource());
   }
 
 }
