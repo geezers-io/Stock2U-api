448f74da4bcc7d65a186fbc2c4501ca97c2d6412
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-16 18:19:37 +0900

    feat: webConfig 추가

--- src/main/java/com/hack/stock2u/global/config/WebConfig.java
+++ src/main/java/com/hack/stock2u/global/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.hack.stock2u.global.config;
 import com.hack.stock2u.authentication.service.AuthVendorEnumConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,4 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addConverter(new AuthVendorEnumConverter());
   }
 
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedHeaders("*")  //모든 Header 허용
+        .allowedOriginPatterns("*") //모든 Origin 허용
+        .allowedMethods("*") //모든 Method 허용
+        .allowCredentials(true) // Credentials 허용
+        .exposedHeaders("Content-Disposition");
+  }
 }
