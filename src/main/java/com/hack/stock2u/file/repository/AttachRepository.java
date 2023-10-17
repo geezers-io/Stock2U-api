baf6808f45dc5b7a9188f799261cd507d41fd15e
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-17 10:09:04 +0900

    feat: 파일 레포지토리 작성

---
+++ src/main/java/com/hack/stock2u/file/repository/AttachRepository.java
@@ -0,0 +1,6 @@
+package com.hack.stock2u.file.repository;
+
+import com.hack.stock2u.models.Attach;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttachRepository extends JpaRepository<Attach, Long> {}
