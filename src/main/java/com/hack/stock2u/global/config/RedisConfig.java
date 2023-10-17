e80e17f31f19943fa7fff341189d2a956de75612
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-17 22:04:31 +0900

    fix: 레디스 직렬화 객체 주입

--- src/main/java/com/hack/stock2u/global/config/RedisConfig.java
+++ src/main/java/com/hack/stock2u/global/config/RedisConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Slf4j
@@ -26,9 +27,12 @@ public class RedisConfig {
 
   @Bean
   public RedisTemplate<?, ?> redisTemplate() {
+    StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
     RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(redisConnectionFactory());
-    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setKeySerializer(stringRedisSerializer);
+    redisTemplate.setHashKeySerializer(stringRedisSerializer);
+    redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
     return redisTemplate;
   }
 
