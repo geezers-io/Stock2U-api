3dacf885092aa84165b1fd94bf8c026abef6bc7c
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-18 12:00:10 +0900

    fix: checkstyle

--- src/main/java/com/hack/stock2u/product/dto/ProductDetails.java
+++ src/main/java/com/hack/stock2u/product/dto/ProductDetails.java
@@ -21,7 +21,7 @@ public record ProductDetails(
     boolean onlyOneReserve,
     boolean showAccountDetails,
     Date expiredAt,
-    @Schema(description = "예약 상태(예약 한 건만 받기 체크 && 예약 진행 중일 시 표기 됨)",nullable = true)
+    @Schema(description = "예약 상태(예약 한 건만 받기 체크 && 예약 진행 중일 시 표기 됨)", nullable = true)
     ReservationStatus status,
     Double latitude,
     Double longtitude,
