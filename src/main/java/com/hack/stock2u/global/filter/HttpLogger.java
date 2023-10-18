5bec1ea52bb3b771ca530a31221430b103282da8
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-18 18:51:45 +0900

    feat: Http 요청 관련 filter 에 swagger 무시.

--- src/main/java/com/hack/stock2u/global/filter/HttpLogger.java
+++ src/main/java/com/hack/stock2u/global/filter/HttpLogger.java
@@ -1,6 +1,8 @@
 package com.hack.stock2u.global.filter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,11 +11,31 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
 @Component
 public class HttpLogger extends OncePerRequestFilter {
+  private final List<String> excludeAntPaths = Arrays.asList(
+      "/api/swagger-ui/**", "/api/docs/**", "/api/api-docs/**"
+  );
+
+  @Override
+  protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    String requestUri = request.getRequestURI();
+    AntPathMatcher matcher = new AntPathMatcher();
+
+    for (var path : excludeAntPaths) {
+      boolean match = matcher.match(path, requestUri);
+      if (match) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   @Override
   protected void doFilterInternal(
       HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
@@ -28,6 +50,7 @@ public class HttpLogger extends OncePerRequestFilter {
       log.debug("[{}] {}", method, requestUri);
       response.setHeader("X-Trace-Id", traceId);
       MDC.clear();
+      filterChain.doFilter(request, response);
     } catch (Exception ex) {
       log.error("error: {}\n{}", ex.getMessage(), ex.getStackTrace());
     }
