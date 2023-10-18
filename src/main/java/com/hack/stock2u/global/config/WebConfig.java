8e03b6c4c20228dac9680216816ca977450a0b82
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-18 17:41:46 +0900

    chore: 웹 환경설정. 추가

--- src/main/java/com/hack/stock2u/global/config/WebConfig.java
+++ src/main/java/com/hack/stock2u/global/config/WebConfig.java
@@ -1,16 +1,25 @@
 package com.hack.stock2u.global.config;
 
 import com.hack.stock2u.authentication.service.AuthVendorEnumConverter;
+import com.hack.stock2u.global.filter.RoleGuardInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+  private final RoleGuardInterceptor roleGuardInterceptor;
 
   @Override
   public void addFormatters(FormatterRegistry registry) {
     registry.addConverter(new AuthVendorEnumConverter());
   }
 
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(roleGuardInterceptor);
+  }
 }
