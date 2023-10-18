bcdbf9074637c74b1ffcf4b5363dd3ad3e5d04e8
HyukJunJi <whrnjsrkdls@gmail.com>
2023-10-18 23:03:34 +0900

    fix: ReservationResponse값 수정

--- src/main/java/com/hack/stock2u/chat/service/ChatRoomService.java
+++ src/main/java/com/hack/stock2u/chat/service/ChatRoomService.java
@@ -2,16 +2,20 @@ package com.hack.stock2u.chat.service;
 
 
 import com.hack.stock2u.chat.dto.request.ChatRoomRequestDto;
-import com.hack.stock2u.chat.dto.request.ReservationRequest;
+import com.hack.stock2u.chat.dto.response.ReservationResponse;
 import com.hack.stock2u.chat.repository.JpaChatRoomRepository;
 import com.hack.stock2u.chat.repository.MessageChatMongoRepository;
+import com.hack.stock2u.file.repository.JpaAttachRepository;
 import com.hack.stock2u.global.exception.GlobalException;
+import com.hack.stock2u.models.Attach;
 import com.hack.stock2u.models.Product;
+import com.hack.stock2u.models.ProductImage;
 import com.hack.stock2u.models.Reservation;
 import com.hack.stock2u.models.User;
 import com.hack.stock2u.product.repository.JpaProductRepository;
 import com.hack.stock2u.user.UserException;
 import com.hack.stock2u.user.repository.JpaUserRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,22 +28,26 @@ public class ChatRoomService {
   private final JpaChatRoomRepository jpaChatRoomRepository;
   private final JpaUserRepository userRepository;
   private final JpaProductRepository productRepository;
+  private final JpaAttachRepository attachRepository;
 
-  public ReservationRequest createRoom(
+
+  public ReservationResponse createRoom(
       ChatRoomRequestDto.CreateReservationRequest createRequest
   ) {
     User purchaser = userRepository.findById(createRequest.purchaserId())
         .orElseThrow(UserException.NOT_FOUND_USER::create);
     Product product = productRepository.findById(createRequest.productId())
         .orElseThrow(GlobalException.NOT_FOUND::create);
-
+    List<Attach> attaches = product.getProductImages().stream()
+        .map(ProductImage::getAttach)
+        .toList();
     Reservation newReservation = jpaChatRoomRepository.save(Reservation.builder()
         .purchaser(purchaser)
         .product(product)
         .seller(product.getSeller())
         .build());
 
-    return ReservationRequest.reserv(newReservation);
+    return ReservationResponse.reserv(newReservation, attaches);
   }
   //  특정 채팅방 불러오기 ->>>>>>>>>할일
   //  @Transactional
