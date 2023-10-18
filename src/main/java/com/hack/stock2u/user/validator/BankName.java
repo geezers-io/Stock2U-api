be0a7348e367541ae32e564a476ac95898b9de89
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-19 05:40:59 +0900

    fix: validator 오류 수정

--- src/main/java/com/hack/stock2u/user/validator/BankName.java
+++ src/main/java/com/hack/stock2u/user/validator/BankName.java
@@ -5,10 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = BankNameValidator.class)
 public @interface BankName {
   String message() default "존재하지 않는 은행입니다.";
+  Class<?>[] groups() default {};
+  Class<? extends Payload>[] payload() default {};
 }
