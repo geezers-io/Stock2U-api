42278a9eb3f0961167bcc44149b1f4874afb938b
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-19 23:16:34 +0900

    feat: 필요 예외 케이스 추가

--- src/main/java/com/hack/stock2u/authentication/AuthException.java
+++ src/main/java/com/hack/stock2u/authentication/AuthException.java
@@ -16,7 +16,7 @@ public enum AuthException implements BasicErrorCase {
   IS_ANONYMOUS_USER(HttpStatus.UNAUTHORIZED, "ANONYMOUS_USER", "인증되지 않는 요청입니다."),
 
   DORO_TOO_LONG(HttpStatus.BAD_REQUEST, "TOO_LONG_SEARCH", "검색어가 너무 깁니다. (한글40자, 영문,숫자 80자 이하)"),
-  DORO_CHARACTER(HttpStatus.BAD_REQUEST, "BAD_CHARACTER", "검색어는 문자와 숫자 같이 입력되어야 합니다. (숫자만 검색 불가)"),
+  DORO_CHARACTER(HttpStatus.BAD_REQUEST, "BAD_CHARACTER", "검색어는 문자와 숫자가 같이 입력되어야 합니다. (숫자만 검색 불가)"),
   DORO_ONLY_ONE_CHAR(HttpStatus.BAD_REQUEST, "ONLY_ONE_CHAR", "검색어는 한글자 이상 입력되어야 합니다."),
   DORO_NOT_INPUT(HttpStatus.BAD_REQUEST, "NOT_INPUT", "검색어가 입력되지 않았습니다."),
   DORO_OVER_SEARCHING(HttpStatus.BAD_REQUEST, "OVERSEARCHING", "검색 범위를 초과하였습니다."),
