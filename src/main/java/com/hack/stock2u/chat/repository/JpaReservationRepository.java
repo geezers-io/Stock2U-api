788a68f6a145166e8d9be395e042423a052f7ee0
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-19 23:15:45 +0900

    fix: 예약 레포지토리 메서드 추가

--- src/main/java/com/hack/stock2u/chat/repository/JpaReservationRepository.java
+++ src/main/java/com/hack/stock2u/chat/repository/JpaReservationRepository.java
@@ -2,6 +2,7 @@ package com.hack.stock2u.chat.repository;
 
 import com.hack.stock2u.models.Reservation;
 import io.lettuce.core.dynamic.annotation.Param;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,4 +10,14 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaReservationRepository extends JpaRepository<Reservation, Long> {
+  /**
+   * 구매자와 연관된 예약리스트를 불러옵니다.
+   */
+  List<Reservation> findByPurchaserId(Long id);
+
+  /**
+   * 판매자와 연관된 예약리스트 불러옵니다.
+   */
+  List<Reservation> findBySellerId(Long id);
+
 }
