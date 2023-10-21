6024d0dd036d15036a323a50ced1908f68df4879
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-22 01:13:54 +0900

    fix: verification 값을 vendor 의 유저 식별값으로 변경

--- src/main/java/com/hack/stock2u/authentication/dto/SignInResponse.java
+++ src/main/java/com/hack/stock2u/authentication/dto/SignInResponse.java
@@ -1,7 +1,9 @@
 package com.hack.stock2u.authentication.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
+@Builder
 public record SignInResponse(boolean exists,
                              @Schema(nullable = true, description = "회원가입 페이지에서 사용할 email")
                             String email,
