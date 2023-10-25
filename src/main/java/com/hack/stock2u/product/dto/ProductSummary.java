d973f96cd423826fdb6e419c12a40e286a363169
galaxy4276 <galaxyhi4276@gmail.com>
2023-10-26 07:19:36 +0900

    fix: querydsl

--- src/main/java/com/hack/stock2u/product/dto/ProductSummary.java
+++ src/main/java/com/hack/stock2u/product/dto/ProductSummary.java
@@ -2,8 +2,6 @@ package com.hack.stock2u.product.dto;
 
 import com.hack.stock2u.constant.ProductType;
 import java.util.Date;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 public record ProductSummary(
     Long id, String title, int price, ProductType type, Date expiredAt, Double distance,
