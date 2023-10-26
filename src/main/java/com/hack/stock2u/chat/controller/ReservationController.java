8bab5d2a15d9be4f455c9d2bb811c0490fba56f8
HyukJunJi <whrnjsrkdls@gmail.com>
2023-10-27 00:56:32 +0900

    fix: 승인할때 상태값 넘겨줌

--- src/main/java/com/hack/stock2u/chat/controller/ReservationController.java
+++ src/main/java/com/hack/stock2u/chat/controller/ReservationController.java
@@ -1,9 +1,11 @@
 package com.hack.stock2u.chat.controller;
 
+import com.hack.stock2u.chat.dto.request.ReportRequest;
 import com.hack.stock2u.chat.dto.request.ReservationApproveRequest;
 import com.hack.stock2u.chat.dto.request.ReservationRequestDto;
 import com.hack.stock2u.chat.service.ChatMessageService;
 import com.hack.stock2u.chat.service.ReservationService;
+import com.hack.stock2u.constant.ReservationStatus;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
@@ -36,7 +38,8 @@ public class ReservationController {
   ) {
     Long id = reservationService.createRoom(createReservationRequest);
     //예약 눌렀을때 메세지 보내지는 방식
-    //상태 보내야함 근데 null로 갈꺼 같음 그래서 상태하나 추가해야될듯?
+    //상태 보내야함 근데 null로 갈꺼 같음 그래서 상태 하나 추가해야될듯?
+    //메세지로 ReservationStatus를 넘기는게 맞을까? 구매자가 판매자에게 넘기는 형식임
     chatMessageService.saveAndSendAutoMessage(createReservationRequest, id);
 
     return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -45,12 +48,13 @@ public class ReservationController {
 
   @Operation(summary = "예약 승인 API", description = "예약을 승인 할때 나오는 API + 구매자에게 자동 메세지 발송")
   @PostMapping("/approve")
-  public ResponseEntity<Void> approveReservationApi(ReservationApproveRequest request) {
-    //채팅 발송하기 구현해야함
+  public ResponseEntity<ReservationStatus> approveReservationApi(ReservationApproveRequest request) {
 
-    reservationService.approve(request);
+    //메세지로 상태값 넘기는게 맞는지?
+    //approve에서 상태값을 넘기는게 나을것 같은데 : 일단 넘김
+    ReservationStatus approve = reservationService.approve(request);
     chatMessageService.saveAndSendAutoMessageApprove(request);
-    return ResponseEntity.status(HttpStatus.OK).build();
+    return ResponseEntity.status(HttpStatus.OK).body(approve);
   }
 
   @Operation(summary = "예약 삭제 API", description = "예약을 위한 채팅방을 삭제할때 사용하는 API")
@@ -61,4 +65,10 @@ public class ReservationController {
     return ResponseEntity.status(HttpStatus.OK).build();
   }
 
+  @Operation(summary = "신고 API", description = "채팅방에서 사용자 신고 API")
+  @PostMapping("/room/report")
+  public ResponseEntity<Short> reportUserApi(@RequestBody @Valid ReportRequest request) {
+    Short reportCount = reservationService.report(request);
+    return ResponseEntity.status(HttpStatus.OK).body(reportCount);
+  }
 }
