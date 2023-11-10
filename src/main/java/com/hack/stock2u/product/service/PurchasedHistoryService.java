3ac3daf2825df11fa3d67e47ffb05250ec1dce06
ByeonJinKyu <fullmetal@kakao.com>
2023-11-11 03:23:56 +0900

    feat: 구매 기록 API 작성

---
+++ src/main/java/com/hack/stock2u/product/service/PurchasedHistoryService.java
@@ -0,0 +1,45 @@
+package com.hack.stock2u.product.service;
+
+import com.hack.stock2u.authentication.dto.SessionUser;
+import com.hack.stock2u.authentication.service.SessionManager;
+import com.hack.stock2u.models.Buyer;
+import com.hack.stock2u.models.Product;
+import com.hack.stock2u.user.dto.PurchasedHistory;
+import com.hack.stock2u.user.dto.SearchDate;
+import lombok.RequiredArgsConstructor;
+import com.hack.stock2u.user.repository.JpaBuyerRepository;
+import java.util.Date;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PurchasedHistoryService {
+  private final JpaBuyerRepository buyerRepository;
+  private final SessionManager sessionManager;
+
+  public Page<PurchasedHistory> getHistories(SearchDate searchDate, Pageable pageable) {
+    SessionUser user = sessionManager.getSessionUser();
+    Page<Buyer> buyers = buyerRepository.findByPurchaserId(user.id(), pageable);
+
+    List<PurchasedHistory> histories = buyers.getContent().stream()
+        .map(this::mapToHistory).toList();
+
+    return new PageImpl<>(histories, pageable, histories.size());
+  }
+
+  private PurchasedHistory mapToHistory(Buyer b) {
+    Product product = b.getProduct();
+    return PurchasedHistory.builder()
+        .productId(product.getId())
+        .title(product.getTitle())
+        .name(product.getName())
+        .thumbnailUrl("")
+        .createdAt(new Date())
+        .build();
+  }
+
+}
--- src/main/java/com/hack/stock2u/user/repository/JpaBuyerRepository.java
+++ src/main/java/com/hack/stock2u/user/repository/JpaBuyerRepository.java
@@ -2,11 +2,17 @@ package com.hack.stock2u.user.repository;
 
 import com.hack.stock2u.models.Buyer;
 import com.hack.stock2u.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JpaBuyerRepository extends JpaRepository<Buyer, Long> {
   //  @Query("select count() from buyers b where b.seller.id = :id")
   int countBySeller(User user);
+
+  Page<Buyer> findByPurchaserId(Long userId, Pageable pageable);
 }
