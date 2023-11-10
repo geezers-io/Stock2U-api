63f45e4666a616a072232a82947d0fc58d656c3e
ByeonJinKyu <fullmetal@kakao.com>
2023-11-11 03:35:30 +0900

    fix: checkstyle

--- src/main/java/com/hack/stock2u/user/repository/JpaBuyerRepository.java
+++ src/main/java/com/hack/stock2u/user/repository/JpaBuyerRepository.java
@@ -4,9 +4,7 @@ import com.hack.stock2u.models.Buyer;
 import com.hack.stock2u.models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
